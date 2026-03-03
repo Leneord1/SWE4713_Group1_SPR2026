4bf84a1fa85356c2e54d12f34f1ed7e4dcd43a92
DuckDonni <jonahsmith2010@gmail.com>
2026-03-02 19:50:17 -0500

    created user service

---
+++ frontend/src/services/userService.js
@@ -0,0 +1,11 @@
+import { supabase } from '../supabaseClient';
+
+export async function createUser(email, password) {
+    try {
+        // Empty
+    }
+    catch (error) {
+        // Replace with error message in future
+        console.error('Error creating user:', error);
+    }
+}
--- frontend/src/utils/passwordHash.js
+++ frontend/src/utils/passwordHash.js
@@ -1,5 +1,4 @@
 import bcrypt from 'bcryptjs';
-import { supabase } from '../supabaseClient';
 
 export async function hashPassword(password){
     const hash = await bcrypt.hash(password, 10);
